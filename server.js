af958c241def33d65cbc483cb3c8f23a6eff4e60
KUMAR PRABUDDH <kprabuddh@gmail.com>
2016-11-11 19:46:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/SignUp', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'SignUp.html'));
+});
 
 
 
